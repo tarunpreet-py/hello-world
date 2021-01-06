088ea4ca37806d6ad55342d9ae5b41dea4874812
tarunpreet-py <73122843+tarunpreet-py@users.noreply.github.com>
2021-01-06 17:01:13 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+txt = "Hello World"
+txt2 = "Elephant"
+txt3 = "Lions"
+txt4 = "Oranges"
+tx5 = "WDYM?"
+txt6 = "Rabbits?"
+txt7 = "Donkeys have d*icks?"
+
+def say_it():
+	print(txt[0] + txt2[0] + txt3[0] + txt3[0] + txt4[0] + " " + txt5[0] + txt4[0] + txt6[0] + txt3[0] + txt7[0])
+
+say_it()
